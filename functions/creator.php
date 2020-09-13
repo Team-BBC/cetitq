3a8c81b3b8fc78307a028ec6d42c6e67b194b53b
MEXSlacker <46721097+MEXSlacker@users.noreply.github.com>
2020-09-13 16:34:36 -0500

    Update creator.php
    cambio en la subida, causaba errores en diferente pc

--- functions/creator.php
+++ functions/creator.php
@@ -3,50 +3,45 @@
 require "bakend.php";
 $myObj = new dbConnect();
 
-!isset($_POST) ? die("Access denied") :"";
+!isset($_POST) ? die("Access denied") : "";
 
 if(isset($_POST['ok'])){
     $nombre = $_POST['nombre'];
-    $archivoRecibido = $_FILES['fichero']['name'];
-    $destino = "../ficheros/$nombre.pdf";
+    $file_name = $_FILES['fichero']['name'];
+    $file_tmp = $_FILES['fichero']['tmp_name'];
     $destinoBD = "$nombre.pdf";
 
     $acceptedarr = array("pdf");
-    $extension = pathinfo($archivoRecibido, PATHINFO_EXTENSION);
-    if(!in_array($extension, $acceptedarr)){
-        echo"Verifica que sea .pdf";
+    $extension = pathinfo($file_name, PATHINFO_EXTENSION);
+    $stmt = $myObj->mysqli->prepare('select * from document where sustancia = ?');
+    $stmt->bind_param('s', $nombre);
+
+    $stmt->execute();
+
+    $result = $stmt->get_result();
+    $row =$result->fetch_assoc();
+
+    if($row!=null){
+        die('Este archivo ya existe');
     }else{
-        $stmt = $myObj->mysqli->prepare('select * from document where sustancia = ?');
-        $stmt->bind_param('s', $nombre);
-
-        $stmt->execute();
-
-        $result = $stmt->get_result();
-        $row =$result->fetch_assoc();
-
-        if($row!=null){
-            die('Este archivo ya existe');
-            header("Location:../admin.php");
+        if(!in_array($extension, $acceptedarr)){
+            echo"Verifica que sea .pdf";
         }else{
-            $stmt = $myObj->mysqli->prepare('insert into document(sustancia, url, fecha) values(?, ?, NOW())');
-            $stmt->bind_param('ss', $nombre, $destinoBD);
-
-            $stmt->execute();
-            $result= $stmt->affected_rows;
-            if ($result > 0){
-                if(move_uploaded_file($archivoRecibido, $destino)){
-                    echo '<script>console.log("hi, file uploaded")</script>';
-                    header("Location:../admin.php");
+            if(move_uploaded_file($file_tmp, "../ficheros/".$file_name)){
+                $query = $myObj->mysqli->prepare('insert into document(sustancia, url, fecha) values(?, ?, NOW())');
+                $query->bind_param('ss', $nombre, $destinoBD);
+                $query->execute();
+                $result= $query->affected_rows;
+                if ($result > 0){
+                    header("Location:../admin.php");        
+                }else{
+                    echo '<script type="text/javascript"> alert("Error al subir archivo"); </script> ';
                 }
             }else{
-                echo '<script type="text/javascript"> alert("Error al subir archivo"); </script> ';
+                echo 'Error al copiar archivo en /ficheros';                
             }
         }
-        header("Location:../admin.php");
     }
-    
-
-
 }
 
 ?>
